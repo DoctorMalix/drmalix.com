e759aedb54415a7102fabbaca0a8f124a361933d
PIAIC-220943 <doctormalix@gmail.com>
2023-11-08 22:39:24 +0500

    Errors removed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,12 +16,12 @@ export default function Contact() {
       <p className="mb-3 text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">
       On our About Page, we share our heartfelt journey at HomeoPathy Haven. Our story begins with a passion for natural healing and a deep-rooted belief in the power of homeopathy to restore balance and vitality. Founded by a team of dedicated homeopaths, our vision was to create a haven where individuals could embrace an alternative approach to health, one that focuses on addressing the underlying causes of ailments rather than merely alleviating symptoms.
       </p>
-      <p> &ensp; &ensp;</p>
+      {/* <p> &ensp; &ensp;</p> */}
 
 At HomeoPathy Haven, we have nurtured a culture of compassion and expertise, where each member of our team is committed to your well-being. Our journey is marked by years of studying, practicing, and refining our skills in the field of homeopathy. We take pride in staying updated with the latest advancements and research in the realm of natural healing to provide you with the best care possible.
-<p> &ensp; &ensp;</p>
+{/* <p> &ensp; &ensp;</p> */}
 Our purpose goes beyond merely offering remedies; it's about building a supportive and caring community that empowers individuals to take charge of their health and embrace a holistic lifestyle. We strive to foster a deep sense of trust and transparency in our relationships with our clients, ensuring that you feel heard, understood, and respected throughout your healing journey.
-<p> &ensp; &ensp;</p>
+{/* <p> &ensp; &ensp;</p> */}
 At HomeoPathy Haven, we are united by a shared vision of promoting well-being, and we invite you to be a part of our story as we continue to touch lives and make a positive difference in the world of natural health.
 
       
